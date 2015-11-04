cc4ab2058183197dfac274159db27426fd95edb0
Chris Patuzzo <chris@patuzzo.co.uk>
2015-11-04 20:28:59 +0000

    Revert "Introduce the notion of singletons…"
    This reverts commit ef447540963f537f90822a7d1ac02fc91823d8d4.
    
    Although these tests pass in isolation, they are causing
    side-effects that result in other test failures.

--- spec/sentient/expression/boolean/true_spec.rb
+++ spec/sentient/expression/boolean/true_spec.rb
@@ -7,21 +7,4 @@ RSpec.describe Sentient::Expression::Boolean::True do
     expect(expression.to_dimacs).to eq [["1"]]
     expect(expression.boolean).to eq("1")
   end
-
-  it "is a singleton and uses the same number each time" do
-    expect(described_class.new.boolean).to eq("1")
-    expect(described_class.new.boolean).to eq("1")
-  end
-
-  it "respects register resets, just like everything else" do
-    Sentient::Expression::Boolean::False.new.boolean
-
-    expect(described_class.new.boolean).to eq("2")
-    expect(described_class.new.boolean).to eq("2")
-
-    Sentient::Register.reset
-
-    expect(described_class.new.boolean).to eq("1")
-    expect(described_class.new.boolean).to eq("1")
-  end
 end
